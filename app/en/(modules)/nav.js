1fd3780831b0a4af22321ee84ca69e03051ea9cb
zin <juztz1n@gmail.com>
2023-01-13 22:24:24 +0100

    Fixed slight alignment issue caused by accidental merge.

--- app/en/(modules)/nav.js
+++ app/en/(modules)/nav.js
@@ -3,8 +3,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import React, { useEffect, useState } from 'react'
-
 import en_US from '../en_US.json'
 
 import styles from './navigation.module.css'
@@ -14,12 +12,14 @@ export default function Nav()
 
     return <div className={styles.container}>
         <nav>
-            <Image 
-                alt={en_US.sections.nav.logo.alt}
-                src={en_US.sections.nav.logo.url}
-                width={175}
-                height={60}
-            />
+            <Link href={"/"}>
+                <Image 
+                    alt={en_US.sections.nav.logo.alt}
+                    src={en_US.sections.nav.logo.url}
+                    width={175}
+                    height={60}
+                />
+            </Link>
             {
                 en_US.sections.nav.items.map((item, index) =>
                 {
