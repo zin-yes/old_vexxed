8030bb929a58954cb3127fb0e269a4bd90255432
zin <juztz1n@gmail.com>
2023-02-10 10:14:03 +0100

    Animated cards

--- app/sections/experience-card.tsx
+++ app/sections/experience-card.tsx
@@ -1,28 +1,129 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { motion, useSpring } from 'framer-motion'
+
+import { Montserrat } from '@next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+
 import styles from './experience-card.module.css'
 
 export function Header({ title, icon }:
 {
   title: string,
-  icon: string    
+  icon: 
+  {
+    path: string,
+    alt: string
+  }
 })
 {
-  return <div>
-      <div><h2>{ title }</h2></div>
+  return <div className={styles.header}>
+      <div className={styles.header_title}>
+        <h3 className={montserrat.className}>{ title }</h3>
+      </div>
+      <div className={styles.header_icon}>
+        <svg width={48} height={48}>
+          <path d={icon.path} strokeWidth={"0"} fill={"none"}/>
+        </svg>
+      </div>
     </div>
 }
 
-export default function Card({ title, icon }:
+
+export function Description({ description }:
 {
-  title: string,
-  icon: string    
+  description: string[]
 })
 {
-  return <div className={styles.container}>
-      <div>
-        <Header 
-          title={title}
-          icon={icon}
-        />
-      </div>
+  return <div className={styles.description}>
+      {
+        description.map((item, index) =>
+        {
+          return <p key={index} className={montserrat.className}>{ item }</p>
+        })
+      }
     </div>
+}
+
+
+export default function Module({ header, description }:
+{
+  header:
+  {
+    title: string,
+    icon: 
+    {
+      path: string,
+      alt: string
+    }
+  },
+  description: string[]
+})
+{
+  const [hover, setHover] = useState(false);
+
+  const [percentage_mouse_x, setPercentageMouseX] = useState(0);
+  const [percentage_mouse_y, setPercentageMouseY] = useState(0);
+
+  const spring_rotation_x = useSpring(0);
+  const spring_rotation_y = useSpring(0);
+
+  const onMouseMove = (e: any) => 
+  {
+    let rect = e.currentTarget.getBoundingClientRect();
+    
+    setPercentageMouseX((e.clientX - rect.left) / rect.width);
+    setPercentageMouseY((e.clientY - rect.top) / rect.height);
+  };
+
+  useEffect(() => 
+    {
+      if(hover)
+      {
+        spring_rotation_x.set(percentage_mouse_x * 3 - 1.5);
+        spring_rotation_y.set(-(percentage_mouse_y * 3 - 1.5));
+      }
+      else
+      {
+        spring_rotation_x.set(0);
+        spring_rotation_y.set(0);
+      }
+    }, 
+    [percentage_mouse_x, percentage_mouse_y, hover]
+  );
+
+
+
+  return <>
+      <motion.div 
+        className={hover ? styles.container_hover + " " + styles.container : styles.container}
+        onMouseMove={onMouseMove}
+        onMouseEnter={ () => setHover(true) }
+        onMouseLeave={ () => setHover(false) }
+        drag
+        onDragStart={() => setHover(true)}
+        onDragEnd={() => setHover(false)}
+        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+        dragTransition={{ bounceStiffness: 500, bounceDamping: 100 }}
+        dragElastic={0.1}
+      >
+        <motion.div
+          style={{
+            rotateX: spring_rotation_y,
+            rotateY: spring_rotation_x
+          }}
+        >
+          <Header 
+            title={header.title}
+            icon={header.icon}
+          />
+          <Description 
+            description={description}
+          />
+        </motion.div>
+      </motion.div>
+    </>
 }
--- app/sections/experience.tsx
+++ app/sections/experience.tsx
@@ -2,6 +2,11 @@ import Card from './experience-card'
 
 import styles from './experience.module.css'
 
+import refund_svg from './full-refund-policy.json'
+import revisions_svg from './unlimited-revisions.json'
+import delivery_svg from './14-day-delivery.json'
+import maintenance_svg from './maintenance.json'
+
 export function Header({ heading, subheading }:
 {
   heading: string,
@@ -42,7 +47,85 @@ export default function Section()
         heading={"This is why you should choose us"}
       />
       <CardContainer>
-        <Card />
+        <Card 
+          header=
+          {
+            {
+              title: "Full refund policy",
+              icon:
+              {
+                path: refund_svg.path,
+                alt: "A pictographic depiction of a full refund policy."
+              }
+            }
+          }
+          description=
+          {
+            [
+              "If you are not happy with the service we are providing, get a full refund at any point, no questions asked."
+            ]
+          }
+        />
+        <Card 
+          header=
+          {
+            {
+              title: "Unlimited revisions",
+              icon:
+              {
+                path: revisions_svg.path,
+                alt: "A brush illustration used as a representation for revisions."
+              }
+            }
+          }
+          description=
+          {
+            [
+              "If you're not happy with certain details, don't worry!",
+              "We will keep making changes until you like what you've got."
+            ]
+          }
+        />
+        <Card 
+          header=
+          {
+            {
+              title: "14-day delivery",
+              icon:
+              {
+                path: delivery_svg.path,
+                alt: "A lightning bolt representing fast delivery."
+              }
+            }
+          }
+          description=
+          {
+            [
+              "A high-quality website shouldn’t take you months. That’s why we have a lightning fast 14 day delivery guarantee.",
+              "(revisions not included)"
+            ]
+          }
+        />
+        <Card 
+          header=
+          {
+            {
+              title: "Maintenance",
+              icon:
+              {
+                path: maintenance_svg.path,
+                alt: "Gears with star particles representing maintenance."
+              }
+            }
+          }
+          description=
+          {
+            [
+              "Our affordable hosting subscription includes maintenance.",
+              "If you want to update your content or make any changes to your website in the future, we’ve got you covered."
+            ]
+          }
+        />
       </CardContainer>
     </div>
 }
